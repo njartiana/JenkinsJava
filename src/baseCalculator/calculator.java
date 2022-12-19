e43ff687fa5430e4a10432f4e741085842ecbcdd
njartiana <48241768+njartiana@users.noreply.github.com>
2022-12-19 16:23:19 +0300

    First commit

---
+++ src/baseCalculator/calculator.java
@@ -0,0 +1,14 @@
+package baseCalculator;
+
+public class calculator {
+    public int add(int value_1, int value_2){
+        return value_1 + value_2 ;
+    }
+    public int substraction(int value_1, int value_2){
+        return value_1 - value_2 ;
+    }
+
+    public int multiplication(int value_1, int value_2){
+        return value_1 * value_2;
+    }
+}
---
+++ src/test_calculator.java
@@ -0,0 +1,37 @@
+import baseCalculator.calculator;
+public class test_calculator {
+    calculator calc = new calculator();
+
+
+    public void test_add(){
+        if (calc.add(2,2) == 4){
+            System.out.println("Test addition passed");
+        }
+        else{
+            System.out.println("Error for function addition");
+        }
+    }
+    public void test_substract(){
+        if (calc.substraction(4,2) == 2){
+            System.out.println("Test subtraction passed");
+        }
+        else{
+            System.out.println("Error for function subtraction");
+        }
+    }
+    public void test_multiplication(){
+        if (calc.multiplication(4,2) == 8){
+            System.out.println("Test multiplication passed");
+        }
+        else{
+            System.out.println("Error for function multiplication");
+        }
+    }
+    public static void main(String[] args){
+        test_calculator test = new test_calculator();
+        test.test_add();
+        test.test_substract();
+        test.test_multiplication();
+    }
+}
+
