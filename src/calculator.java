008ed75740057ebd90ccf4464efe240cc04634c8
njartiana <48241768+njartiana@users.noreply.github.com>
2022-12-19 17:10:45 +0300

    First move all test function to calculator

--- src/calculator.java
+++ src/calculator.java
@@ -9,4 +9,36 @@ public class calculator {
     public int multiplication(int value_1, int value_2){
         return value_1 * value_2;
     }
+
+    public void test_add(){
+        if (add(2,2) == 4){
+            System.out.println("Test addition passed");
+        }
+        else{
+            System.out.println("Error for function addition");
+        }
+    }
+    public void test_substract(){
+        if (substraction(4,2) == 2){
+            System.out.println("Test subtraction passed");
+        }
+        else{
+            System.out.println("Error for function subtraction");
+        }
+    }
+    public void test_multiplication(){
+        if (multiplication(4,2) == 8){
+            System.out.println("Test multiplication passed");
+        }
+        else{
+            System.out.println("Error for function multiplication");
+        }
+    }
+
+    public static void main(String[] args){
+        calculator test = new calculator();
+        test.test_add();
+        test.test_multiplication();
+        test.test_substract();
+    }
 }
